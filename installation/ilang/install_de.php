ee4f0b9955898c3820a5221b65c3eb1b16f62cab
Michael Schroeder <michael.p.schroeder@gmail.com>
2008-08-03 00:15:26 +0200

    adding new files from language improvement

---
+++ installation/ilang/install_de.php
@@ -0,0 +1,67 @@
+<?php
+/*
+=================================== 
+zock! 
+ 
+Developed by 
+------------ 
+* Michael Schroeder: 
+   michael.p.schroeder@gmail.com  
+* 
+* 
+ 
+http://zock.sf.net 
+ 
+zock! is a free software licensed under GPL (General public license) v3 
+      more information look in the root folder for "LICENSE". 
+=================================== 
+*/
+
+$lang = Array();
+$lang['../installation/install_title'] = 'Installation';
+
+$lang['instl_welcome'] = 'Willkommen zur Installation von zock! Habe Spass und keine Probleme während des Installierens! =)';
+$lang['instl_exp'] = 'Du musst ALLES korrekt ausfüllen damit die Installation ein Erfolg wird!';
+
+$lang['instl_err_email'] = 'Du musst eine gültige Email-Adresse eingeben';
+$lang['instl_err_prefix'] = 'Du musst ein Präfix auswählen. Und kurz soll es auch sein.';
+$lang['instl_err_server'] = 'Kann mit diesen Parametern keine Verbindung zum Server herstellen. Bitte überprüfe Servername, Benutzername und Passwort.';
+$lang['instl_err_table'] = 'Cannot select the table (connecetion the server si fine tough!).';
+$lang['instl_err_table'] = 'Kann nicht auf die Tabelle zugreifen (Verbindung zum Server scheint aber in Ordnung zu sein!).';
+$lang['instl_err_username'] = 'Du musst einen gültigen Benutzernamen eingeben.';
+$lang['instl_err_varsnotwritable'] = 'Die Datei "src/vars.php" ist nicht beschreibbar! Bitte ändere die Zugriffsrechte für diese Datei!';
+$lang['instl_err_installationnotwritable'] = 'Der Ordner "installation" ist nicht beschreibbar! Bitte ändere die Zugriffsrechte für diese Datei!';
+
+
+
+$lang['instl_db_title'] = 'Datenbank';
+$lang['instl_db_host'] = '<b>Hostname:</b><br/> Dies ist normalerweise "localhost" oder ein Hostname der vom Hoster zur Verfügung gestellt wird.';
+$lang['instl_db_username'] = '<b>Benutzername:</b><br/> Dies kann der Standardbenutzername "root" von MYSQL, ein Name, der vom Hoster zur Verfügung gestellt wird oder einer der du beim Aufsetzten des Datenbankservers kreiert hast.';
+$lang['instl_db_password'] = '<b>Passwort:</b><br/> Für die Seitensicherheit ist es obligatorisch ein Passowrt zu brauchen für das MySQL-Konto. Dies ist das selbe Passwort, dass sie brauchen um auf die Datenbank zuzugreifen. Auch dies könnte von deinem Hoster vorgegeben sein.';
+$lang['instl_db_name'] = '<b>Database Name:</b><br/> The name of the database in which you would like to install the tables for zock!';
+$lang['instl_db_name'] = '<b>Datenbankname:</b><br/> Der Name der Datenbank in welche du die Tabellen von zock! installieren möchtest.';
+$lang['instl_db_prefix'] = '<b>Table Prefix:</b><br/> The prefix is necessary to prevent duplicate table names and to group the tables in your database. It is important that you choose a prefix that doesn\'t exist yet in your database.';
+$lang['instl_db_prefix'] = '<b>Tabellen-Präfix:</b><br/> Das Präfix ist nötig um schon vorhandenen Tabellennamen vorzubeugen und die Tabellen in der Datenbank zu gruppieren. Es ist wichtig, dass du ein Präfix wählst, das noch nicht existiert in der ausgewählten Datenbank.';
+
+$lang['instl_installnow'] = 'Jetzt installieren!';
+
+$lang['instl_user_title'] = 'Dein Benutzerkonto';
+$lang['instl_user_email'] = '<b>Deine Email-Adresse</b>';
+$lang['instl_user_exp'] = 'Du wirst der Administrator dieses Wettbüros sein und ein Konto wird dir automatisch mit der Installation dieses Wettbüros erstellt. <p/> Bitte wähle einen <b>Benutzername</b>. Das Passwort wird "<b>admin</b>" sein. Sobald es installiert ist kannst du Passwort nach erfolgreichem Einloggen ändern.';
+
+$lang['instl_yourbettingoffice'] = "Dein Wettbüro";
+
+$lang['instl_zock_title'] = 'zock!-Einstellungen';
+$lang['instl_zock_name'] = '<b>Name:</b><br/> Der Name deines Wettbüros (der Seite). Wird dort im Seitenkopf angezeigt werden, wo du jetzt "zock!" lesen kannst."';
+$lang['instl_zock_desc'] = '<b>Slogan:</b><br/> Der Slogan deines Wettbüros (deiner Seite). Wird dort im Seitenkopf angezeigt werden, wo du jetzt "Dein Wettbüro" lesen kannst"';
+
+
+$lang['footer_contact'] = 'Kontakt';
+$lang['footer_server_time'] = 'Aktuelle Serverzeit';
+
+$lang['general_bettingOffice'] = 'Wettbüro';
+$lang['general_yes'] = 'Ja';
+$lang['general_no'] = 'Nein';
+
+
+
